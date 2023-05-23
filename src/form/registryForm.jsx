0117d0a141af82826f249111afb7ac1d78a76ea3
Carth501 <carth501@gmail.com>
2023-05-23 16:16:10 -0500

    attempt api call

--- src/form/registryForm.jsx
+++ src/form/registryForm.jsx
@@ -6,17 +6,29 @@ import './registryForm.css';
 
 export default function RegistryForm () {
   const [phoneNumber, setPhoneNumber] = useState();
-  const [registrationTexts, setRegistrationTexts] = useState(null);
-  const [registrationCalls, setRegistrationCalls] = useState(null);
-  const [electionTexts, setElectionTexts] = useState(null);
-  const [electionCalls, setElectionCalls] = useState(null);
+  const [registrationTexts, setRegistrationTexts] = useState();
+  const [registrationCalls, setRegistrationCalls] = useState();
+  const [electionTexts, setElectionTexts] = useState();
+  const [electionCalls, setElectionCalls] = useState();
+  const [data, setData] = useState();
+
+  React.useEffect(() => {
+    console.log('requesting data');
+    fetch("http://localhost:3001/api")
+      .then((result) => result.json())
+      .then((data) => {console.log(data); setData(data.message)});
+  }, []);
 
   function handlePhoneNumberChange (newValue) {
     setPhoneNumber(newValue);
   }
 
   function handleSubmission() {
-    if(validatePhoneNumber(phoneNumber)) {
+    if(typeof phoneNumber === 'undefined')
+    {
+      return;
+    } 
+    if( validatePhoneNumber(phoneNumber)) {
       console.log('Success!');
       return;
     }
@@ -51,6 +63,7 @@ export default function RegistryForm () {
         <Choice medium='phone calls' value={electionCalls} setValue={setElectionCalls} />
       </div>
       <Button variant="contained" size="large" onClick={handleSubmission}>submit</Button>
+      <p>{!data ? "Loading..." : data}</p>
     </div>
   )
 }
